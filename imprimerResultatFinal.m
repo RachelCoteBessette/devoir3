8533c9a4b0822d550a0f8bc67a73e4cd3313c10b
Clarence Desmarais <clarencedesmarais@hotmail.com>
2017-11-13 15:23:18 -0500

    ajout dune fonction pour imprimer

---
+++ imprimerResultatFinal.m
@@ -0,0 +1,16 @@
+function imprimerResultatFinal(numCas, Col1, tf, raf, vaf, rbf, vbf)
+
+disp(strcat('------ Essai ', numCas, '------'));
+disp('Col1');
+disp(Col1);
+disp('tf');
+disp(tf);
+disp('raf');
+disp(raf);
+disp('vaf');
+disp(vaf);
+disp('rbf');
+disp(rbf);
+disp('vbf');
+disp(vbf);
+graphique();
